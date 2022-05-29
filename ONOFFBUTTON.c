efb3679b213a81ad44d5c045f94efb84e52388fb
Anokhautomation <67797819+anokhramesh@users.noreply.github.com>
2022-05-29 23:34:26 +0400

    Update ONOFFBUTTON.c

--- ONOFFBUTTON.c
+++ ONOFFBUTTON.c
@@ -1,23 +1,23 @@
-
+// anokhramesh@gmail.com
   void main() {
 
-     trisb.f0=1;
-     trisb.f1=1;
-     trisb.f2=0;
-     portb.f2=0;
+     trisb.f0=1; // set port B 0 as an input
+     trisb.f1=1; // set port B 1 as an input
+     trisb.f2=0; // set port B 2 as an output
+     portb.f2=0;  //set default walue of port B is LOW
 
 
      while(1)
      {
-            if(portb.f0==1)
+            if(portb.f0==1)  //if value of port B 0 is equal to 1 or HIGH
             {
-                portb.f2=1;
+                portb.f2=1; //set the value of Port B2 to High
 
 
             }
-            if(portb.f1==1)
+            if(portb.f1==1) //if value of port B 1 is equal to 1 or HIGH
             {
-               portb.f2=0;
+               portb.f2=0; //set the value of Port B2 to Low
             }
 
 
